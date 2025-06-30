a2e0b24c571ef0d0673f947a27e7e7973a3d6013
moralesmejia <mejiajosemanuel.abc@gmail.com>
2025-06-30 13:33:26 -0600

    fix: usar node en producción

--- routes/formulario.routes.js
+++ routes/formulario.routes.js
@@ -1,17 +1,24 @@
 import express from 'express';
-import * as controlador from '../controllers/formulario.controller.js';
+import {
+  crearFormulario,
+  obtenerFormularios,
+  obtenerFormularioPorId,
+  actualizarFormulario,
+  eliminarFormulario,
+  crearDesdeExcel
+} from '../controllers/formulario.controller.js';
 import upload from '../middlewares/upload.middleware.js';
 
 const router = express.Router();
 
-// Rutas específicas primero
-router.post('/upload', upload.single('archivo'), controlador.crearDesdeExcel);
+// Rutas REST
+router.post('/', crearFormulario);
+router.get('/', obtenerFormularios);
+router.get('/:id', obtenerFormularioPorId);
+router.put('/:id', actualizarFormulario);
+router.delete('/:id', eliminarFormulario);
 
-// Rutas CRUD
-router.post('/', controlador.crearFormulario);
-router.get('/', controlador.obtenerFormularios);
-router.get('/:id', controlador.obtenerFormularioPorId);
-router.put('/:id', controlador.actualizarFormulario);
-router.delete('/:id', controlador.eliminarFormulario);
+// Subida de archivo Excel
+router.post('/upload', upload.single('archivo'), crearDesdeExcel);
 
 export default router;
