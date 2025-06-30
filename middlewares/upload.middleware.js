42b696d2f57703ee905129ed74a35cf998168f6d
moralesmejia <mejiajosemanuel.abc@gmail.com>
2025-06-30 11:56:40 -0600

    fix: usar node en producción

--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -1,6 +1,8 @@
-const multer = require('multer');
+import multer from 'multer';
 
+// Usamos almacenamiento en memoria, pero puedes cambiar a disco si lo deseas
 const storage = multer.memoryStorage();
+
 const upload = multer({ storage });
 
-module.exports = upload;
+export default upload;
